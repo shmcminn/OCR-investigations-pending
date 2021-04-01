61445705adc3bb05479e22a006fbedf67781efc8
Sean McMinn <seanpm92@gmail.com>
2021-04-01 10:47:08 -0400

    more loggin

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
 import requests
-
+print('import requests')
 resp = requests.get('https://www2.ed.gov/about/offices/list/ocr/docs/investigations/open-investigations/tix.html?perPage=1000&sorts%5Binstitution-type%5D=-1')
+print('have response')
 
 with open("output.html", 'w') as ofile:
 	ofile.write(resp.text)
+ofile.close()
+
+print('wrote file')
