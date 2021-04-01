b0be0079a82a1745726269a5a8eba80fdbb12acb
Sean McMinn <seanpm92@gmail.com>
2021-04-01 11:09:00 -0400

    parse into a json

--- scraper.py
+++ scraper.py
@@ -1,11 +1,36 @@
-print("running")
+import json
+
 import requests
-print('import requests')
+from bs4 import BeautifulSoup
+
 resp = requests.get('https://www2.ed.gov/about/offices/list/ocr/docs/investigations/open-investigations/tix.html?perPage=1000&sorts%5Binstitution-type%5D=-1')
-print('have response')
+
+soup = BeautifulSoup(resp.text, 'html.parser')
+
+table = soup.select("#oi-table")[0]
+
+header_atags = table.select('thead a')
+
+export_data = []
+
+headers = []
+
+for atag in header_atags:
+	headers.append(atag.get_text())
+
+rows = table.select('tbody tr')
+
+for row in rows:
+	d = {} 
+	for ind, td in enumerate(row.select('td')):
+		d[headers[ind]] = td.get_text()
+
 
 with open("output.html", 'w') as ofile:
 	ofile.write(resp.text)
 ofile.close()
 
-print(resp.text)
+with open("output.json", 'w') as ofile:
+	ofile.write(json.dumps(export_data))
+ofile.close()
+
