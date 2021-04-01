8417c6757415cc78752c8f7ee773b9b23c76788c
Sean McMinn <seanpm92@gmail.com>
2021-04-01 10:48:20 -0400

    EVEN More loggin

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+print("running")
 import requests
 print('import requests')
 resp = requests.get('https://www2.ed.gov/about/offices/list/ocr/docs/investigations/open-investigations/tix.html?perPage=1000&sorts%5Binstitution-type%5D=-1')
